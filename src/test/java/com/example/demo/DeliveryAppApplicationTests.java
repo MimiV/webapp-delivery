11edee623bb669143ee2795f56793619a8275294
vasconcelosm1atwit <71852288+vasconcelosm1atwit@users.noreply.github.com>
2021-03-06 03:35:38 -0500

    first commit

---
+++ src/test/java/com/example/demo/DeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
